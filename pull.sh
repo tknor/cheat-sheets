41331896553011bdb83e06dd048877a63ed34c4e
Grinder <crimsondeed@gmail.com>
2019-10-17 00:05:17 +0200

    -

--- pull.sh
+++ pull.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
 git pull
+
+read -p "Press ENTER to continue"
--- push.sh
+++ push.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
 git add .
 git commit -m "-"
 git push origin master
+
+read -p "Press ENTER to continue"
