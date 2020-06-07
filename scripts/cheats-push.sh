fbfb62d658a997633aa923bb883e7443f041447e
Grinder <crimsondeed@gmail.com>
2020-06-07 02:01:20 +0200

    auto push

--- scripts/cheats-push.sh
+++ scripts/cheats-push.sh
@@ -4,10 +4,35 @@
 
 script_start
 
+phase "copying private scripts"
+
 cp -r $CONST_SCRIPTS_LOCAL_FOLDER $CONST_SCRIPTS_LOCAL_BACKUP_FOLDER
+
+phase "commiting and pushing public cheat-sheets with scripts"
 
 cd $CONST_CHEATS_FOLDER
 git status
-git add .
-git commit -a -m "auto push"
-git push origin master
+
+echo "Continue'?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git add .; git status; break;;
+        No ) exit;;
+    esac
+done
+
+echo "Continue'?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git commit -a -m "auto push"; break;;
+        No ) exit;;
+    esac
+done
+
+echo "Continue'?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git push origin master; break;;
+        No ) exit;;
+    esac
+done
