17fddaabc2cd7fe6179bc2fa7ba192492dea2a67
tomas.knor <tomas.knor@cgi.com>
2021-03-05 17:07:00 +0100

    auto push

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -9,31 +9,37 @@ if [[ -v ffc ]]; then
     $ffs/./git-push-specific.sh $ffc
 fi
 
-header1 "jvm-lab"
+header1 "jvm-lab (java, kotlin, groovy, with tools)"
 
 if [[ -v ffjl ]]; then
     $ffs/./git-push-specific.sh $ffjl
 fi
 
-header1 "nodejs-lab"
+header1 "nodejs-lab (typescript, javascript, with tools)"
 
 if [[ -v ffnl ]]; then
     $ffs/./git-push-specific.sh $ffnl
 fi
 
-header1 "spring-lab"
+header1 "spring-lab (spring, kotlin)"
 
 if [[ -v ffsl ]]; then
     $ffs/./git-push-specific.sh $ffsl
 fi
 
-header1 "aranea (spring)"
+header1 "celeri (spring, kotlin, for vps)"
+
+if [[ -v ffcel ]]; then
+    $ffs/./git-push-specific.sh $ffcel
+fi
+
+header1 "aranea (spring, java, for framework testing)"
 
 if [[ -v ffara ]]; then
     $ffs/./git-push-specific.sh $ffara
 fi
 
-header1 "ankylos (angular)"
+header1 "ankylos (typescript, angular, for vps)"
 
 if [[ -v ffank ]]; then
     $ffs/./git-push-specific.sh $ffank
